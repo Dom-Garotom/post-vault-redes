b6894e6bcd25c024412d2c4493a699871a462b41
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2025-01-15 17:53:37 -0300

    feat: creating components of post

--- src/components/molecula/post/index.tsx
+++ src/components/molecula/post/index.tsx
@@ -1,38 +1,18 @@
-import React from 'react'
-
 import { PostStyled } from './postStyled'
-import UserImage from "../../../assets/user-image.png"
-import PopoverPost from '../../atomo/popover'
+import UserInfoInPost from './userInfoInPost'
 
 type PostProps = {
-    userName : string,
-    userInfo : string,
+    userId: number,
+    postTitle: string,
+    body: string
 }
 
-
-export default function Post() {
+export default function Post({ postTitle, body, userId }: PostProps) {
     return (
         <PostStyled.Wrapper>
-
-            <PostStyled.WrapperInfo>
-                <div className='user-info'>
-                    <img src={UserImage} width={60} height={60} alt="Imagem do usuário" />
-                    <div>
-                        <h3>User</h3>
-                        <span>location</span>
-                    </div>
-                </div>
-                <PopoverPost/>
-            </PostStyled.WrapperInfo>
-
-            <PostStyled.TitlePost>How to Create a Zig-Zag Box Using CSS</PostStyled.TitlePost>
-
-            <PostStyled.ParagraphPost>
-                TLDRLearn how to create stylish Zig-Zag borders using modern CSS techniques.
-                This tutorial walks through the HTML and CSS code needed
-                to achieve two different Zig-Zag box designs.....
-            </PostStyled.ParagraphPost>
-
+            <UserInfoInPost userId={userId} />
+            <PostStyled.TitlePost>{postTitle}</PostStyled.TitlePost>
+            <PostStyled.ParagraphPost>{body}</PostStyled.ParagraphPost>
         </PostStyled.Wrapper>
     )
 }
