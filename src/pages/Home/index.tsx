73d1510935c8caab8a7788bcf045382aad935fa1
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2025-01-20 11:12:31 -0300

    feat: implementing the post list on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PostList from '../../components/molecula/postList'
+
+import { Wrapper } from './style'
 
 const Home: React.FC = () => {
-  return <div>Home</div>
+  return (
+      <Wrapper.body>
+        <PostList />
+      </Wrapper.body>
+  )
 }
 
 export default Home
