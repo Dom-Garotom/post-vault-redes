a11a0e397a7b20651fcbdc102dccaa7bddf1f5fd
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2025-01-20 11:10:24 -0300

    feat: creating the component that will wrap  the post list

---
+++ src/components/molecula/postList/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+
+import { PostContainer, PostContainerInfo } from './style'
+import Post from '../post'
+import { Post as TypePost } from '../../../types/post'
+import { fetchData } from '../../../utils/fetchData'
+import { postWithUserInfo } from '../../../types/postWithUser'
+import { mergePostWithUser } from '../../../utils/mergePostWithUser'
+import { User } from '../../../types/userApi'
+import PopoverPost from '../popover'
+
+export default function PostList() {
+  const [listPostWithUserInfo, setListPostWithUserInfo] = useState<
+    postWithUserInfo[] | undefined
+  >([])
+
+  useEffect(() => {
+    fetchAndOrderData()
+  }, [])
+
+  const fetchAndOrderData = async () => {
+    try {
+      const [userResponse, postResponse] = await Promise.all([
+        fetchData<User>('/users'),
+        fetchData<TypePost>('/posts'),
+      ])
+
+      if (!userResponse || !postResponse) {
+        throw new Error('Não foi possível realizar a requisição dos dados')
+      }
+
+      const postList = postResponse
+      const userList = userResponse
+
+      // Retorna um objeto com as informações dos post atrelado com as informações dos usuários que fez o post
+      const listPostWithUser = mergePostWithUser(postList, userList)
+      setListPostWithUserInfo(listPostWithUser)
+    } catch (error) {
+      console.log('Error : ' + error)
+    }
+  }
+
+  return (
+    <PostContainer>
+      <PostContainerInfo>
+        <span>For You</span>
+        <PopoverPost />
+      </PostContainerInfo>
+
+      {listPostWithUserInfo?.map((post) => (
+        <Post
+          key={post.id}
+          userName={post.username}
+          email={post.email}
+          postTitle={post.title}
+          body={post.body}
+        />
+      ))}
+    </PostContainer>
+  )
+}
