b37693a8c4b6e334abb7e537c894e661ddf2629f
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2025-01-27 10:16:49 -0300

    feat: creating the dropDown menu component

--- src/components/molecula/popover/index.tsx
+++ src/components/molecula/popover/index.tsx
@@ -1,10 +1,31 @@
+import { useState } from 'react'
+
+import ConfigImage from '../../../assets/sliders-solid.svg'
+import TrashImage from '../../../assets/trash.svg'
 import { PopoverStyled } from './popoverStyled'
-import ConfigImage from "../../../assets/sliders-solid.svg"
+import { DropDown } from '../../atomo/dropDown'
 
 export default function PopoverPost() {
+  const [isClicked, setIsClicked] = useState(false)
+
   return (
-    <PopoverStyled.Popover>
+    <PopoverStyled.Wrapper>
+      <PopoverStyled.Button onClick={() => setIsClicked(!isClicked)}>
         <img src={ConfigImage} width={20} height={20} />
-    </PopoverStyled.Popover>
+      </PopoverStyled.Button>
+
+      {isClicked && (
+        <DropDown.Menu>
+          <DropDown.MenuItem>
+            <img src={ConfigImage} width={20} height={20} />
+            Edit post
+          </DropDown.MenuItem>
+          <DropDown.MenuItem>
+            <img src={TrashImage} width={20} height={20} />
+            Exclude post
+          </DropDown.MenuItem>
+        </DropDown.Menu>
+      )}
+    </PopoverStyled.Wrapper>
   )
 }
